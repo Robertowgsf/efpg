4dd4712f00a01ab3ae9734ad361ed68d36341d6b
Roberto Wagner <robertowgsf@gmail.com>
2021-11-09 11:09:16 -0300

    Add project files.

---
+++ EfPg/Migrations/20211109140637_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace EfPg.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "blogs",
+                columns: table => new
+                {
+                    blog_id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_blogs", x => x.blog_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "posts",
+                columns: table => new
+                {
+                    post_id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(nullable: true),
+                    content = table.Column<string>(nullable: true),
+                    blog_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_posts", x => x.post_id);
+                    table.ForeignKey(
+                        name: "fk_posts_blogs_blog_id",
+                        column: x => x.blog_id,
+                        principalTable: "blogs",
+                        principalColumn: "blog_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_posts_blog_id",
+                table: "posts",
+                column: "blog_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "posts");
+
+            migrationBuilder.DropTable(
+                name: "blogs");
+        }
+    }
+}
